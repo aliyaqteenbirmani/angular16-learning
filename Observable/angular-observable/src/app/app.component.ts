9865dc387304d5f556cc598eee42ba11ca6b8618
Ali Yaqteen <aliyaqteenbirmani512@gmail.com>
2025-08-27 08:58:48 +0500

    Observeable, Observer, next, error & complete in Angular

--- Observable/angular-observable/src/app/app.component.ts
+++ Observable/angular-observable/src/app/app.component.ts
@@ -8,17 +8,20 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'angular-observable';
-  data:any[] = [];
+  data:number[] = [];
 
   // 1. Create an observable 
   // Observable
   myObservable = new Observable ((observer) =>
   {
     setTimeout(() => {observer.next(1)},1000);
-    setTimeout(() => {observer.next(2)},3000);
-    setTimeout(() => {observer.next(3)},5000);
-    setTimeout(() => {observer.next(4)},7000);
-    setTimeout(() => {observer.next(5)},9000);
+    setTimeout(() => {observer.next(2)},2000);
+    setTimeout(() => {observer.next(3)},3000);
+    setTimeout(() => {observer.next(4)},4000);
+    // setTimeout(() => {observer.error(new Error('Something went wrong'))},4000);
+    setTimeout(() => {observer.next(5)},5000);
+    setTimeout(() => {observer.complete()},6000);
+
 
   });
 
@@ -26,8 +29,26 @@ export class AppComponent {
 
     // Observer
     // next, error, complete
-    this.myObservable.subscribe((val:any) => {
+  //   this.myObservable.subscribe((val:any) => {
+  //     this.data.push(val);
+  //   },
+  // (err) => {
+  //   alert(err.message);
+  // }, () => {
+  //   alert('Completed');
+  // });
+
+  this.myObservable.subscribe({
+    next: (val:any) => {
       this.data.push(val);
-    });
+    },
+    error: (err) => {
+      alert(err.message);
+    },
+    complete: () => {
+      alert('Completed');
+    }
+  });
+
   }
 }
